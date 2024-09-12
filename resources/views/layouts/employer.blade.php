9f36253d3f111bbf6c7030769c0e8448c28623f3
JRMHD <reaganmukabana@gmail.com>
2024-09-13 01:23:57 +0300

    initial commit

--- resources/views/layouts/employer.blade.php
+++ resources/views/layouts/employer.blade.php
@@ -310,7 +310,7 @@
                 <li class="nav-item">
                     <a href="#" class="nav-link">
                         <i class="fas fa-wallet"></i>
-                        <span>Payments</span>
+                        <span>Wallet</span>
                     </a>
                 </li>
                 <li class="nav-item">
